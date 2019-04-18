1b08b2758390e03ff760c7177198c0595593fb53
Joseph Kapp <joseph.a.kapp@gmail.com>
2019-04-17 23:27:41 -0400

    Updated main activity, created new activity for animation selection

---
+++ app/src/main/java/com/example/rgbarduinocontrollerandroid/AnimationSelection.java
@@ -0,0 +1,18 @@
+package com.example.rgbarduinocontrollerandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class AnimationSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_selection);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
